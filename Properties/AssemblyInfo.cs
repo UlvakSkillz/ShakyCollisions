3fdcea13c54de190073330e33443769de54ccb79
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-01-23 16:34:13 -0800

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using ShakyCollisions;
+
+[assembly: MelonInfo(typeof(main), "Shaky Collisions", "1.0.0", "UlvakSkillz")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+[assembly: AssemblyTitle("ShakyCollisions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShakyCollisions")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("cc94f205-27d2-4bb4-bbb4-05905d12e30a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ main.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+using RUMBLE.MoveSystem;
+using UnityEngine;
+
+namespace ShakyCollisions
+{
+    public class main : MelonMod
+    {
+        private CombatManager combatManager;
+        private string currentScene = "";
+        private bool sceneChanged = false;
+
+        public override void OnFixedUpdate()
+        {
+            if ((currentScene == "Loader") && (sceneChanged))
+            {
+                try
+                {
+                    combatManager = GameObject.Find("Game Instance/Other/CombatManager").GetComponent<CombatManager>();
+                    combatManager.structureImpactShakeStrength *= 5;
+                    sceneChanged = false;
+                }
+                catch { }
+            }
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            currentScene = sceneName;
+            sceneChanged = true;
+        }
+    }
+}
