96086d7e5655c27b69aa7a5bee3b48c7758024c9
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2026-03-04 06:57:49 -0800

    2.0.1

--- main.cs
+++ main.cs
@@ -1,33 +1,17 @@
 ﻿using MelonLoader;
 using Il2CppRUMBLE.MoveSystem;
-using UnityEngine;
 
 namespace ShakyCollisions
 {
     public class main : MelonMod
     {
-        private CombatManager combatManager;
-        private string currentScene = "";
-        private bool sceneChanged = false;
-
-        public override void OnFixedUpdate()
-        {
-            if ((currentScene == "Loader") && (sceneChanged))
-            {
-                try
-                {
-                    combatManager = CombatManager.instance;
-                    combatManager.structureImpactShakeStrength *= 5;
-                    sceneChanged = false;
-                }
-                catch { }
-            }
-        }
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            currentScene = sceneName;
-            sceneChanged = true;
+            if (sceneName == "Loader")
+            {
+                CombatManager.instance.structureImpactShakeStrength *= 5;
+            }
         }
     }
 }
