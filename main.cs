94d797135e82e8cdad03408a5e787c13456e5edf
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2026-04-10 23:47:31 -0700

    2.2.0

--- main.cs
+++ main.cs
@@ -10,12 +10,12 @@ namespace ShakyCollisions
         public static class BuildInfo
         {
             public const string ModName = "Shaky Collisions";
-            public const string ModVersion = "2.1.0";
+            public const string ModVersion = "2.2.0";
             public const string Author = "UlvakSkillz";
         }
 
         private Mod ShakyCollisions = new Mod();
-        List<ModSetting> settings = new List<ModSetting>();
+        private List<ModSetting> settings = new List<ModSetting>();
 
         public override void OnLateInitializeMelon()
         {
@@ -27,8 +27,9 @@ namespace ShakyCollisions
             ShakyCollisions.ModName = BuildInfo.ModName;
             ShakyCollisions.ModVersion = BuildInfo.ModVersion;
             ShakyCollisions.SetFolder(BuildInfo.ModName);
-            settings.Add(ShakyCollisions.AddToList("Enabled", false, 0, "Toggles Mod On/Off", new Tags { }));
-            settings.Add(ShakyCollisions.AddToList("Shake Strength", 1.875f, "Controls how Shaky the Structures are. 0.375 is the Game's Default.", new Tags { }));
+            settings.Add(ShakyCollisions.AddToList("Enabled", true, 0, "Toggles Mod On/Off", new Tags { }));
+            settings.Add(ShakyCollisions.AddToList("Shake Strength", 1.875f, "Controls how Hard the Shake of the Structures are. 0.375 is the Game's Default.", new Tags { }));
+            settings.Add(ShakyCollisions.AddToList("Shake Frequency", 150f, "Controls how often it Shakees the Structures. 75 is the Game's Default.", new Tags { }));
             ShakyCollisions.GetFromFile();
             Save();
             ShakyCollisions.ModSaved += Save;
@@ -37,8 +38,10 @@ namespace ShakyCollisions
 
         private void Save()
         {
-            if ((bool)settings[0].SavedValue) { CombatManager.instance.structureImpactShakeStrength = (float)settings[1].SavedValue; } //turn on
-            else { CombatManager.instance.structureImpactShakeStrength = 0.375f; } //turn off
+            if ((bool)settings[0].SavedValue) { CombatManager.instance.structureImpactShakeStrength = (float)settings[1].SavedValue; } //turn on Strength
+            else { CombatManager.instance.structureImpactShakeStrength = 0.375f; } //turn off Strength
+            if ((bool)settings[0].SavedValue) { CombatManager.instance.structureImpactShakeFrequency = (float)settings[2].SavedValue; } //turn on Frequency
+            else { CombatManager.instance.structureImpactShakeFrequency = 75f; } //turn off Frequency
         }
     }
 }
